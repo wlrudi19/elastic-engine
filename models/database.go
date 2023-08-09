65b1d4f56c50c53778ef25235adfb7e74ec96b97
Rudi Wildantoro Lesmana <rudi.lesmana@amartha.com>
2023-08-09 14:27:41 +0700

    EE-03: add user & product models

--- models/database.go
+++ models/database.go
@@ -11,3 +11,24 @@ type DatabaseConfig struct {
 	Username string
 	Password string
 }
+
+type Users struct {
+	Users UsersConfig
+}
+
+type Product struct {
+	Product ProductConfig
+}
+
+type UsersConfig struct {
+	User_id int
+	Name    string
+	Email   string
+}
+type ProductConfig struct {
+	Product_id  int
+	Name        string
+	Description string
+	Amount      string
+	Stok        int
+}
