c52765bf2d270ecccb8998d1a5a645533dda717d
Rudi Wildantoro Lesmana <wl.rudi19@gmail.com>
2023-08-24 00:25:50 +0700

    EE-07: update product-2

--- app/product/api/route.go
+++ app/product/api/route.go
@@ -11,11 +11,11 @@ func NewProductRouter(productHandler ProductHandler) *chi.Mux {
 	authMiddleware := middlewares.Authenticate
 
 	router.Route("/api/products", func(r chi.Router) {
-		r.With(authMiddleware).Get("/create", productHandler.CreateProductHandler)
+		r.With(authMiddleware).Post("/create", productHandler.CreateProductHandler)
 		r.Get("/findbyId", productHandler.FindProductHandler)
 		r.Get("/findall", productHandler.FindProductAllHandler)
-		r.With(authMiddleware).Get("/deletebyId", productHandler.DeleteProductHandler)
-		r.With(authMiddleware).Get("/update/{product_id}", productHandler.UpdateProductHandler)
+		r.With(authMiddleware).Put("/deletebyId", productHandler.DeleteProductHandler)
+		r.With(authMiddleware).Put("/update/{product_id}", productHandler.UpdateProductHandler)
 	})
 
 	return router
